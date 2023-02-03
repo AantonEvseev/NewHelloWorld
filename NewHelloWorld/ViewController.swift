651ef9b9b6bd8d8db2eaa4b48f8750f313044a38
AantonEvseev <97557052+AantonEvseev@users.noreply.github.com>
2023-02-03 12:51:41 +0200

    Initial Commit

---
+++ NewHelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewHelloWorld
+//
+//  Created by Anton Evseev on 03.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
